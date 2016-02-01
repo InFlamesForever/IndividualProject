1a140efbf1b40d208a99f1a62d8a5896f5c5fa7b
rcame <rcame@essex.ac.uk>
2016-02-01 02:43:29 +0000

    Comment changes

--- utilities/textures.h
+++ utilities/textures.h
@@ -6,12 +6,12 @@
 //Char texture
 extern Texture gDotTexture;
 
-//Dirt textures
+//Dirt textures -- 3 Textures
 extern Texture gDirt_DirtTexture;
 extern Texture gDirt_DirtGravelTexture;
 extern Texture gDirt_GravelTexture;
 
-//Grass textures
+//Grass textures -- 6 Textures
 extern Texture gGrass_DeadTexture;
 extern Texture gGrass_DryTexture;
 extern Texture gGrass_DyingTexture;
@@ -19,27 +19,27 @@ extern Texture gGrass_LushDeepTexture;
 extern Texture gGrass_LushLightTexture;
 extern Texture gGrass_ParchedTexture;
 
-//Cobblestone pavement texture
+//Cobblestone pavement texture -- 1 Texture
 extern Texture gPavement_CobblestoneTexture;
 
-//Sand textures
+//Sand textures -- 2 textures
 extern Texture gSand_DarkTexture;
 extern Texture gSand_LightTexture;
 
-//Gray stone textures
+//Gray stone textures -- 5 textures
 extern Texture gStone_Gray_DarkTexture;
 extern Texture gStone_Gray_LightTexture;
 extern Texture gStone_Gray_MediumTexture;
 extern Texture gStone_Gray_VeryDarkTexture;
 extern Texture gStone_Gray_VeryLightTexture;
 
-//Sandstone textures
+//Sandstone textures -- 4 textures
 extern Texture gStone_Sand_DarkTexture;
 extern Texture gStone_Sand_LightTexture;
 extern Texture gStone_Sand_MediumTexture;
 extern Texture gStone_Sand_VeryLightTexture;
 
-//Water textures
+//Water textures -- 3 textures
 extern Texture gWater_RiverTexture;
 extern Texture gWater_SeaTexture;
 extern Texture gWater_SmallStreamTexture;
