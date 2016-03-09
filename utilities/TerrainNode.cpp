51395bd37e982a7e3e902aef10c315229b8bb63a
rcame <rcame@essex.ac.uk>
2016-03-09 20:42:54 +0000

    Updated the node class for improved search

--- utilities/TerrainNode.cpp
+++ utilities/TerrainNode.cpp
@@ -8,6 +8,11 @@ TerrainNode::TerrainNode(int x, int y, TerrainNode* prev){
     this->x = x;
     this->y = y;
     previousNode = prev;
+    if(prev == NULL){
+        disFromStart = 0;
+    } else {
+        disFromStart = prev->getDistFromStart()+1;
+    }
 }
 
 int TerrainNode::getX() {
@@ -24,4 +29,8 @@ TerrainNode* TerrainNode::getPrevNode() {
 
 TerrainNode::~TerrainNode() {
 
+}
+
+int TerrainNode::getDistFromStart() {
+    return disFromStart;
 }
--- utilities/TerrainNode.h
+++ utilities/TerrainNode.h
@@ -14,14 +14,16 @@ using namespace std;
 
 class TerrainNode {
 public:
-    TerrainNode(int x, int y, TerrainNode* prev = NULL);
+    TerrainNode(int x, int y,TerrainNode* prev = NULL);
     ~TerrainNode();
     int getX();
     int getY();
+    int getDistFromStart();
     TerrainNode* getPrevNode();
 private:
     int x;
     int y;
+    int disFromStart = 0;
     TerrainNode* previousNode;
 };
 
