03fca23beebebb4bdcc49e41cd6167a41e3e2039
rcame <rcame@essex.ac.uk>
2016-03-14 08:04:50 +0000

    Initial Upload

---
+++ Code/Characters/Enemies/Slime.cpp
@@ -0,0 +1,21 @@
+//
+// Created by richard on 14/03/16.
+//
+
+#include "Slime.h"
+
+Slime::Slime(int playerLevel, bool isBoss, int posX, int posY)
+        : EnemyCharacter(posX, posY){
+    Texture* texts[3] = {
+            &gEnemy_Slime,
+            &gEnemy_Slime_Angry,
+            &gEnemy_Slime_Attack
+    };
+    setTextures(texts);
+
+    setVars(playerLevel, isBoss, BASE_HEALTH,
+            BASE_ATTACK, BASE_DEFENCE, BASE_MOVE_SPEED,
+            BASE_EXP,
+            MULTIPLIER_HEALTH, MULTIPLIER_ATTACK,
+            MULTIPLIER_DEFENCE, MULTIPLIER_EXP);
+}
---
+++ Code/Characters/Enemies/Slime.h
@@ -0,0 +1,31 @@
+//
+// Created by richard on 14/03/16.
+//
+
+#ifndef INDIVIDUALPROJECT_SLIME_H
+#define INDIVIDUALPROJECT_SLIME_H
+
+#include "../EnemyCharacter.h"
+
+/*
+ * Slime is a low health, low attack and high defence enemy
+ */
+class Slime: public EnemyCharacter {
+public:
+    Slime(int playerLevel, bool isBoss, int posX, int posY);
+
+private:
+    const int BASE_HEALTH = 50;
+    const int BASE_ATTACK = 5;
+    const int BASE_DEFENCE = 30;
+    const int BASE_MOVE_SPEED = 1;
+    const int BASE_EXP = 10;
+
+    const int MULTIPLIER_HEALTH = 5;
+    const int MULTIPLIER_ATTACK = 2;
+    const int MULTIPLIER_DEFENCE = 10;
+    const int MULTIPLIER_EXP = 5;
+};
+
+
+#endif //INDIVIDUALPROJECT_SLIME_H
