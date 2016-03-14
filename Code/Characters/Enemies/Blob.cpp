bfee3c2b1a9979059312a5ea6f29db25aac47a67
rcame <rcame@essex.ac.uk>
2016-03-14 07:50:23 +0000

    Moduralised the generation of characters

--- Code/Characters/Enemies/Blob.cpp
+++ Code/Characters/Enemies/Blob.cpp
@@ -13,21 +13,8 @@ Blob::Blob(int playerLevel, bool isBoss, int posX, int posY)
     };
     setTextures(texts);
 
-    int level = playerLevel + randInteger(-2, 2);
-    int health;
-    int attack;
-    int defence;
-
-    if(isBoss) {
-        health = 1000 + 100 * level;
-        attack = 100 + 50 * level;
-        defence = 250 + 50 * level;
-        moveSpeed = level;
-    } else {
-        health = 100 + 10 * level;
-        attack = 10 + 5 * level;
-        defence = 25 + 5 * level;
-        moveSpeed = 5 + level;
-    }
-    setVars(health, level, attack, defence);
+    setVars(playerLevel, isBoss, BASE_HEALTH,
+            BASE_ATTACK, BASE_DEFENCE, BASE_MOVE_SPEED,
+            MULTIPLIER_HEALTH, MULTIPLIER_ATTACK,
+            MULTIPLIER_DEFENCE);
 }
--- Code/Characters/Enemies/Blob.h
+++ Code/Characters/Enemies/Blob.h
@@ -11,6 +11,16 @@
 class Blob: public EnemyCharacter {
 public:
     Blob(int playerLevel, bool isBoss, int posX, int posY);
+
+private:
+    const int BASE_HEALTH = 100;
+    const int BASE_ATTACK = 10;
+    const int BASE_DEFENCE = 20;
+    const int BASE_MOVE_SPEED = 5;
+
+    const int MULTIPLIER_HEALTH = 10;
+    const int MULTIPLIER_ATTACK = 5;
+    const int MULTIPLIER_DEFENCE = 5;
 };
 
 
