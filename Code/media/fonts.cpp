6ceb734b1431fbc6ab687e5254f9163727864d9d
rcame <rcame@essex.ac.uk>
2016-03-14 02:49:00 +0000

    Added fonts

---
+++ Code/media/fonts.cpp
@@ -0,0 +1,10 @@
+//
+// Created by richard on 14/03/16.
+//
+
+#include "fonts.h"
+
+TTF_Font* gCantarell_Regular;
+TTF_Font* gCantarell_Bold;
+TTF_Font* gCantarell_Oblique;
+TTF_Font* gCantarell_Oblique_Bold;
---
+++ Code/media/fonts.h
@@ -0,0 +1,20 @@
+//
+// Created by richard on 14/03/16.
+//
+
+#ifndef INDIVIDUALPROJECT_FONTS_H
+#define INDIVIDUALPROJECT_FONTS_H
+
+
+//-----------------------------------------------------------------------------
+//########################## Fonts ###########################################
+//-----------------------------------------------------------------------------
+
+#include <SDL_ttf.h>
+
+extern TTF_Font* gCantarell_Regular;
+extern TTF_Font* gCantarell_Bold;
+extern TTF_Font* gCantarell_Oblique;
+extern TTF_Font* gCantarell_Oblique_Bold;
+
+#endif //INDIVIDUALPROJECT_FONTS_H
